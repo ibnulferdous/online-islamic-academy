05cc5a4a2c424bdbe153bea83caf2766b031159e
Ibnul Ferdous <ibnul04@gmail.com>
2021-10-04 15:54:48 +0600

    Home Page design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Home.css';
+import qualityLearning from '../../images/quality-learning-02.png';
+import flexibleTiming from '../../images/flexible-timing-02.png';
+import exclusiveGuideline from '../../images/exclusive-guideline-02.png';
 
 const Home = () => {
     return (
@@ -7,11 +10,34 @@ const Home = () => {
             <header>
                 <div className="overlay-div">
                     <div className="container h-100 d-flex justify-content-center align-items-center flex-column">
-                        <h1 className="text-white display-4 text-center">And We have certainly made the Quran easy for remembrance, so is there any who will remember?</h1>
+                        <h1 className="text-white display-5 text-center">And We have certainly made the Quran easy for remembrance, so is there any who will remember?</h1>
                         <p className="text-white text-center mt-3 h4 fw-light">- Surah Al-Qamar: 17, 22, 32, 40</p>
+                        <div>
+                            <button type="button" class="btn btn-lg btn-danger mx-2 my-5">All courses</button>
+                            <button type="button" class="btn btn-lg btn-outline-light mx-2 my-5">About us</button>
+                        </div>
                     </div>
                 </div>
             </header>
+            <div className="container py-5 features-div">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-5">
+                    <div className="col text-center">
+                        <img className="features-icon" src={qualityLearning} alt="" />
+                        <h2>Quality Learning</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit fuga ex sapiente cumque cum ducimus culpa voluptatem obcaecati</p>
+                    </div>
+                    <div className="col text-center">
+                        <img className="features-icon" src={flexibleTiming} alt="" />
+                        <h2>Flexible Timing</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dolore officia dignissimos expedita nam excepturi laboriosam fuga voluptate harum</p>
+                    </div>
+                    <div className="col text-center">
+                        <img className="features-icon" src={exclusiveGuideline} alt="" />
+                        <h2>Quality Content</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur perspiciatis, ipsam earum, quas consequatur quos labore placeat vero maiores</p>
+                    </div>
+                </div>
+            </div>
             <h1 className="display-1 text-center py-5">Display Hello World</h1>
             <h1 className="display-1 text-center py-5">Display Hello World</h1>
             <h1 className="display-1 text-center py-5">Display Hello World</h1>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import logo from '../../images/logo.png';
+import logo from '../../logo.png';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
@@ -8,10 +8,10 @@ const Navigation = () => {
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light header-menu">
                 <div class="container-fluid">
-                    <a class="navbar-brand d-flex align-items-center">
-                        <img src={logo} alt="OIA" width="50" class="d-inline-block align-text-top" />
+                    <Link class="navbar-brand d-flex align-items-center" to="/">
+                        <img src={logo} alt="OIA" width="35" class="d-inline-block align-text-top" />
                         <span className="fw-bold m-2">Online Islamic Academy</span>
-                    </a>
+                    </Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -30,6 +30,8 @@ const Navigation = () => {
                                 <Link class="nav-link" to="/cart">Cart</Link>
                             </li>
                         </ul>
+                        <button type="button" class="btn btn-danger mx-3">Log in</button>
+                        <button type="button" class="btn btn-outline-dark">Sign up</button>
                     </div>
                 </div>
             </nav>
