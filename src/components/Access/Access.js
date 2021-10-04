81f8a49065c90217a026a464164e837637ab2fed
Ibnul Ferdous <ibnul04@gmail.com>
2021-10-04 22:14:55 +0600

    Editing Readme file

--- src/components/Access/Access.js
+++ src/components/Access/Access.js
@@ -5,12 +5,51 @@ const Access = () => {
     return (
         <main className="access-div">
             <div className="container py-5">
-                <div className="row row-cols-1 row-cols-md-2 g-5 d-flex align-items-center">
+                <div className="row row-cols-1 row-cols-md-2 g-5">
                     <div className="col-md">
-                        <h3 className="display-5 pb-5 mb-5 text-center">Log in</h3>
+                        <h3 className="display-5 pb-5 mb-1 text-center">Log in</h3>
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" class ="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" />
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class ="form-check-label" for="exampleCheck1">Keep me logged in</label>
+                            </div>
+                            <button type="submit" class="btn btn-danger">Submit</button>
+                        </form>
                     </div>
                     <div className="col-md">
-                        
+                        <h3 className="display-5 pb-5 mb-1 text-center">Sign up</h3>
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Full name</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" />
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Re-enter Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" />
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label" for="exampleCheck1">I agree to the terms and conditions</label>
+                            </div>
+                            <button type="submit" class="btn btn-outline-secondary">Submit</button>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import pageNotFoundIcon from '../../images/page-not-found.png';
+import './PageNotFound.css';
 
 const PageNotFound = () => {
     return (
-        <div className="container py-5 text-center">
-            <h1 className="display-1">404: Page not Found!</h1>
+        <div className="page-not-found-div">
+            <div className="container py-5 text-center">
+                <img src={pageNotFoundIcon} alt="Page not found icon" />
+                <h1 className="display-1">
+                    <span className="text-danger">404:</span> Page not Found!
+                </h1>
+            </div>
         </div>
+        
     );
 };
 
